3858cfeb17559025f14e89ecc27e43727c328d8d
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-01-21 17:37:48 +0000

    finished all stretch goals

--- index.js
+++ index.js
@@ -228,10 +228,19 @@ Use the carMaker function below to do the following:
 */
 
 
-function carMaker(/* code here */) {
-    /* code here */
-    
+function carMaker(odometer) {
+  return {
+    odometer: odometer,
+    drive: function(distance) {
+      this.odometer += distance;
+      return this.odometer;
+    }
+  };
 }
+
+const car1 = carMaker(10);
+console.log(car1);
+console.log(car1.drive(100));
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
